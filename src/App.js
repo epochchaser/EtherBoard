d4be97c70ebe48225452ce2b1f7a1696e24a0a4a
chaser <epochchaser@gmail.com>
2018-06-11 03:36:04 +0900

    add material-ui/core, material-ui/icons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,124 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { NavLink } from 'react-router-dom';
+import Routes from './Routes';
+
+/*
+              <NavbarNav className="ml-auto">
+                <NavItem>
+                  <NavLink className="nav-link" to="/">Home</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/css">CSS</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/components">Components</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/javascript">JavaScript</NavLink>
+                </NavItem>
+              </NavbarNav>
+*/
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 
 class App extends Component {
+  state = {
+    auth: true,
+    anchorEl: null,
+  };
+
+  handleChange = (event, checked) => {
+    this.setState({ auth: checked });
+  };
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
   render() {
+    const { auth, anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
     return (
-      <div className="App">
-        
-      </div>
+      <Router>
+        <div>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton color="inherit" aria-label="Menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="title" color="inherit" >
+                ETHEREUM BOARD
+              </Typography>
+              {auth && (
+                <div>
+                  <IconButton
+                    aria-owns={open ? 'menu-appbar' : null}
+                    aria-haspopup="true"
+                    onClick={this.handleMenu}
+                    color="inherit"
+                  >
+                    <AccountCircle />
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    open={open}
+                    onClose={this.handleClose}
+                  >
+                    <MenuItem containerElement={<NavLink to={"/"}/>}>Profile</MenuItem>
+                    <MenuItem containerElement={<NavLink to={'/signup'}/>}>SignUp</MenuItem>
+                  </Menu>
+                </div>
+              )}
+            </Toolbar>
+          </AppBar>
+
+          <main style={{marginTop: '4rem'}}>
+            <Routes />
+          </main>
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
